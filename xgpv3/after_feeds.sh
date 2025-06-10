b3418f33d2b6eebafb62b50fbca1361779950070
micc <36817721+miccjing@users.noreply.github.com>
2025-06-10 20:25:16 +0800

    Update after_feeds.sh

--- xgpv3/after_feeds.sh
+++ xgpv3/after_feeds.sh
@@ -4,6 +4,7 @@
 #添加passwall
 rm -rf feeds/luci/applications/luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall2 package/openwrt-passwall2
 #ttyd默认登陆root
 sed -i "s|option command '/bin/login'|option command '/bin/login -f root'|g" feeds/packages/utils/ttyd/files/ttyd.config
 #修改turboacc配置
