b6241b1593e290f1f1e91e4d5fbd1677622cdc85
micc <36817721+miccjing@users.noreply.github.com>
2025-09-14 23:34:19 +0800

    Update after_feeds.sh

--- xgpv3/after_feeds.sh
+++ xgpv3/after_feeds.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # 脚本用途：在 OpenWrt feeds 更新和安装后执行的后续处理脚本
 #
-#添加passwall
+# 移除 openwrt feeds 自带的核心库
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
+# 移除 openwrt feeds 过时的luci版本
 rm -rf feeds/luci/applications/luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall2 package/openwrt-passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
 #ttyd默认登陆root
 sed -i "s|option command '/bin/login'|option command '/bin/login -f root'|g" feeds/packages/utils/ttyd/files/ttyd.config
 #修改turboacc配置
