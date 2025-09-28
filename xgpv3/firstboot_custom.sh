c9ce6f3ad30d958f00fbf335cf4ba538294200e3
micc <36817721+miccjing@users.noreply.github.com>
2025-09-28 21:00:28 +0800

    Update firstboot_custom.sh

--- xgpv3/firstboot_custom.sh
+++ xgpv3/firstboot_custom.sh
@@ -29,6 +29,8 @@ wifi reload
 uci set qmodem.main.enable_pcie_scan='1'
 #qmodem启用USB模组扫描
 uci set qmodem.main.enable_usb_scan='1'
+#开机时延时启动(扫描)
+uci set qmodem.main.start_delay='12'
 uci commit qmodem
 # 删除自身，防止重复执行
 rm -f /etc/uci-defaults/999-firstboot_custom.sh
