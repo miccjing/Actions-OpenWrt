ca10f1e8e9b39ec77e0c54bc17018d3fd0fd8466
micc <36817721+miccjing@users.noreply.github.com>
2025-11-11 21:37:26 +0800

    Update before_feeds.sh

--- xgpv3/before_feeds.sh
+++ xgpv3/before_feeds.sh
@@ -2,7 +2,6 @@
 # 脚本用途：在 OpenWrt feeds 更新之前执行的准备工作脚本
 #
 # 添加Qmodem feeds
-git reset --hard e1a584b
 echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
 # 添加luci-app-turboacc
 # curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
