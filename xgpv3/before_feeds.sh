44ae61b847df898ac23996e9fe520663530caf8a
micc <36817721+miccjing@users.noreply.github.com>
2026-04-10 20:56:25 +0800

    Update before_feeds.sh

--- xgpv3/before_feeds.sh
+++ xgpv3/before_feeds.sh
@@ -4,7 +4,7 @@
 # 添加Qmodem feeds
 echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
 # 添加luci-theme-alpha
-echo 'src-git alpha https://github.com/miccjing/luci-theme-alpha.git;main' >> feeds.conf.default
+git clone https://github.com/miccjing/luci-theme-alpha package/luci-theme-alpha
 # 添加luci-app-easytier
 # git clone https://github.com/EasyTier/luci-app-easytier package/luci-app-easytier
 # 添加luci-app-passwall2
