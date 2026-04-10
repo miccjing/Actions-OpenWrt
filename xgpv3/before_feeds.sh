53a1c3cbfbaad55589801c1fae97acfbfb28981d
micc <36817721+miccjing@users.noreply.github.com>
2026-04-10 20:38:59 +0800

    Update before_feeds.sh

--- xgpv3/before_feeds.sh
+++ xgpv3/before_feeds.sh
@@ -3,8 +3,6 @@
 #
 # 添加Qmodem feeds
 echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
-# 修改默认密码为password
-sed -i 's/root:::0:99999:7:::/root:$5$.I1gWK7dcUcq0vLu$6hBUt6cPnCk3.GVLcUvJOVdcrHm7RbbiXBNCfvdufBD:20231:0:99999:7:::/g' package/base-files/files/etc/shadow
 # 添加luci-theme-alpha
 git clone https://github.com/miccjing/luci-theme-alpha package/luci-theme-alpha
 # 添加luci-app-easytier
