6aa0b6442249d67c58b2b9353038d47b3857b2b0
micc <36817721+miccjing@users.noreply.github.com>
2026-05-21 23:48:18 +0800

    Update after_feeds.sh

--- xgpv3/after_feeds.sh
+++ xgpv3/after_feeds.sh
@@ -2,3 +2,6 @@
 # 脚本用途：在 OpenWrt feeds 更新和安装后执行的后续处理脚本
 # 取消 luci-app-attendedsysupgrade
 # sed -i '/luci-app-attendedsysupgrade/d' feeds/luci/collections/luci/Makefile
+#替换geodata
+rm -rf feeds/packages/net/v2ray-geodata/*
+curl -L https://raw.githubusercontent.com/Openwrt-Passwall/openwrt-passwall-packages/refs/heads/main/v2ray-geodata/Makefile -o feeds/packages/net/v2ray-geodata/Makefile
