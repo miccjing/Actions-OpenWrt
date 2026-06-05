e46d5e182e18d802f3ec0c1216fa2ba4523eab9a
micc <36817721+miccjing@users.noreply.github.com>
2026-06-05 18:00:42 +0800

    Update after_feeds.sh

--- xgpv3/after_feeds.sh
+++ xgpv3/after_feeds.sh
@@ -2,6 +2,8 @@
 # 脚本用途：在 OpenWrt feeds 更新和安装后执行的后续处理脚本
 # 取消 luci-app-attendedsysupgrade
 # sed -i '/luci-app-attendedsysupgrade/d' feeds/luci/collections/luci/Makefile
-#替换geodata
+# 替换geodata
 rm -rf feeds/packages/net/v2ray-geodata/*
 curl -L https://raw.githubusercontent.com/Openwrt-Passwall/openwrt-passwall-packages/refs/heads/main/v2ray-geodata/Makefile -o feeds/packages/net/v2ray-geodata/Makefile
+# 修改设备名
+sed -i "s|model = "NLnet XiGuaPi V3";|model = "WatermelonPi V3";|g" target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/rk3568-xiguapi-v3.dts
