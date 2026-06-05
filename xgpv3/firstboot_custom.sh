953f3e89d8f2fefd46d9f67ed7d926fb5d0bddeb
micc <36817721+miccjing@users.noreply.github.com>
2026-06-05 17:42:42 +0800

    Update firstboot_custom.sh

--- xgpv3/firstboot_custom.sh
+++ xgpv3/firstboot_custom.sh
@@ -10,13 +10,17 @@ uci set wireless.@wifi-iface[0].key='j1472580369'
 # uci set wireless.radio0.country='CN'
 # uci set wireless.radio0.cell_density='0'
 # 4. 修改 LuCI 主题
-uci set luci.main.mediaurlbase='/luci-static/alpha'
+# uci set luci.main.mediaurlbase='/luci-static/alpha'
 # 5. 关闭 LED（避免重复添加）
 uci add system led
 uci set system.@led[-1].name='disable_led'
 uci set system.@led[-1].sysfs='blue:status'
 uci set system.@led[-1].trigger='none'
 uci set system.@led[-1].default='0'
+# qmodem设置
+uci set qmodem.main.block_auto_probe='1'
+uci del qmodem.main.enable_pcie_scan
+uci del qmodem.main.enable_usb_scan
 # 重新加载
 uci commit wireless
 wifi reload
