907f08543b46a32dac7fdf9caeaf45a397e9e71b
micc <36817721+miccjing@users.noreply.github.com>
2026-09-06 21:46:26 +0800

    Update firstboot_custom.sh

--- redmiax6/firstboot_custom.sh
+++ redmiax6/firstboot_custom.sh
@@ -13,16 +13,6 @@ uci set wireless.@wifi-iface[0].key='j1472580369'
 # uci set wireless.radio0.cell_density='0'
 # 修改 LuCI 主题
 # uci set luci.main.mediaurlbase='/luci-static/alpha'
-# 关闭 LED
-#uci add system led
-#uci set system.@led[-1].name='disable_led'
-#uci set system.@led[-1].sysfs='blue:status'
-#uci set system.@led[-1].trigger='none'
-#uci set system.@led[-1].default='0'
-# qmodem设置(修复T99W373)
-#uci set qmodem.main.block_auto_probe='1'
-#uci del qmodem.main.enable_pcie_scan
-#uci del qmodem.main.enable_usb_scan
 # 重新加载
 uci commit wireless
 wifi reload
