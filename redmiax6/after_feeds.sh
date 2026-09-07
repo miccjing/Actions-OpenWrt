efb3d5b3d4b6e4ed190fc26319718398819873e7
micc <36817721+miccjing@users.noreply.github.com>
2026-09-07 19:01:11 +0800

    Update after_feeds.sh

--- redmiax6/after_feeds.sh
+++ redmiax6/after_feeds.sh
@@ -3,5 +3,5 @@
 # 取消 luci-app-attendedsysupgrade
 # sed -i '/luci-app-attendedsysupgrade/d' feeds/luci/collections/luci/Makefile
 # 替换geodata
-#rm -rf feeds/packages/net/v2ray-geodata/*
-#curl -L https://raw.githubusercontent.com/Openwrt-Passwall/openwrt-passwall-packages/refs/heads/main/v2ray-geodata/Makefile -o feeds/packages/net/v2ray-geodata/Makefile
+rm -rf feeds/packages/net/v2ray-geodata/*
+curl -L https://raw.githubusercontent.com/Openwrt-Passwall/openwrt-passwall-packages/refs/heads/main/v2ray-geodata/Makefile -o feeds/packages/net/v2ray-geodata/Makefile
