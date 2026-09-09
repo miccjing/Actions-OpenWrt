4f1f24f18316e2392193920be1fa14ce732d3870
micc <36817721+miccjing@users.noreply.github.com>
2026-09-09 21:05:08 +0800

    Update before_feeds.sh

--- redmiax6/before_feeds.sh
+++ redmiax6/before_feeds.sh
@@ -5,3 +5,5 @@
 git clone https://github.com/EasyTier/luci-app-easytier package/luci-app-easytier
 # 添加luci-app-passwall2
 echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' >> feeds.conf.default
+# 添加luci-theme-material3
+git clone https://github.com/KawaiiHachimi/luci-theme-material3 package/luci-theme-material3
