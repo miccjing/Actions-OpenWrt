2a9313ecb00d1875c84bbf506b6fc42165324335
micc <36817721+miccjing@users.noreply.github.com>
2026-09-09 21:05:23 +0800

    Update before_feeds.sh

--- redmiax6/before_feeds.sh
+++ redmiax6/before_feeds.sh
@@ -2,7 +2,7 @@
 # 脚本用途：在 OpenWrt feeds 更新之前执行的准备工作脚本
 #
 # 添加luci-app-easytier
-git clone https://github.com/EasyTier/luci-app-easytier package/luci-app-easytier
+# git clone https://github.com/EasyTier/luci-app-easytier package/luci-app-easytier
 # 添加luci-app-passwall2
 echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' >> feeds.conf.default
 # 添加luci-theme-material3
