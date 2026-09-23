0b319ea498752e344b554b5ccbc8cc40349091d2
micc <36817721+miccjing@users.noreply.github.com>
2026-09-23 17:25:09 +0800

    Update firstboot_custom.sh

--- redmiax5jdc/firstboot_custom.sh
+++ redmiax5jdc/firstboot_custom.sh
@@ -5,17 +5,10 @@ sed -i "s|option command '/bin/login'|option command '/bin/login -f root'|g" /et
 # sed -i "s|option enabled '0'|option enabled '1'|g" /etc/config/xray 
 # 设置 root 密码
 printf "password\npassword\n" | passwd root
-# 配置 WiFi
-uci set wireless.@wifi-iface[0].ssid='OpenWrt'
-uci set wireless.@wifi-iface[0].encryption='psk2+ccmp'
-uci set wireless.@wifi-iface[0].key='j1472580369'
-# uci set wireless.radio0.country='CN'
-# uci set wireless.radio0.cell_density='0'
 # 修改 LuCI 主题
-# uci set luci.main.mediaurlbase='/luci-static/alpha'
-# 重新加载
-uci commit wireless
-wifi reload
+# uci set luci.main.mediaurlbase='/luci-static/
+uci set luci.main.lang='zh_cn'
+uci set luci.main.mediaurlbase='/luci-static/material3'
 uci commit luci
 uci commit system
 exit 0
