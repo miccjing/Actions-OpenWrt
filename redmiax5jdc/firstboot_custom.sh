0bb69aadf52893e11128febc83f21e1f897ee5c3
micc <36817721+miccjing@users.noreply.github.com>
2026-09-24 06:22:14 +0800

    Update firstboot_custom.sh

--- redmiax5jdc/firstboot_custom.sh
+++ redmiax5jdc/firstboot_custom.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # ttyd 自动登录 root
-sed -i "s|option command '/bin/login'|option command '/bin/login -f root'|g" /etc/config/ttyd
+# sed -i "s|option command '/bin/login'|option command '/bin/login -f root'|g" /etc/config/ttyd
 # 开启 xray
 # sed -i "s|option enabled '0'|option enabled '1'|g" /etc/config/xray 
 # 设置 root 密码
