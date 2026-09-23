d87b361643c777118e23e711ffdceaab99d14ded
micc <36817721+miccjing@users.noreply.github.com>
2026-09-24 06:46:06 +0800

    Update after_feeds.sh

--- redmiax6/after_feeds.sh
+++ redmiax6/after_feeds.sh
@@ -5,3 +5,4 @@
 # 替换geodata
 rm -rf feeds/packages/net/v2ray-geodata/*
 curl -L https://raw.githubusercontent.com/Openwrt-Passwall/openwrt-passwall-packages/refs/heads/main/v2ray-geodata/Makefile -o feeds/packages/net/v2ray-geodata/Makefile
+sed -i "s/option min_cache_ttl '3600'/option min_cache_ttl '0'/" package/network/services/dnsmasq/files/dhcp.conf
